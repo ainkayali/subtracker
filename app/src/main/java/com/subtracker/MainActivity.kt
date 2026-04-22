c2782b9cb3f46ece5b89290cb78e9c1f67388959
ainkayali <ainkayali@users.noreply.github.com>
2026-04-22 11:19:56 +0000

    Fix imports: BackHandler and setContent from activity-compose

--- app/src/main/java/com/subtracker/MainActivity.kt
+++ app/src/main/java/com/subtracker/MainActivity.kt
@@ -2,6 +2,8 @@ package com.subtracker
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
+import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -13,7 +15,6 @@ import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.BackHandler
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
